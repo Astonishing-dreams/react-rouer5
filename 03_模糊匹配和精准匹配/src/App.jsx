a67d05f1927c125e386588d2070812b1407bf2cd
惊梦 <1449432824@qq.com>
2022-07-22 22:50:01 +0800

    模糊匹配和精准匹配

---
+++ 03_模糊匹配和精准匹配/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import MyNavLink from './components/MyNavLink'
+
+export default function App() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-xs-8">
+          <div className="page-header"><h2>React Router Demo</h2></div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            {/* <a className="list-group-item" href="./about.html">About</a>
+            <a className="list-group-item active" href="./home.html">Home</a> */}
+            <MyNavLink to="/a/about">About</MyNavLink>
+            <MyNavLink to="/a/home">Home</MyNavLink>
+            {/* 模糊匹配 <MyNavLink to="/a/home/b/c">Home</MyNavLink> */}
+
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body">
+              {/* 注册路由 */}
+              <Switch>
+                {/* 开启精准匹配 */}
+                <Route exact path='/a/about' component={About}></Route>
+                <Route exact path='/a/home' component={Home}></Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
